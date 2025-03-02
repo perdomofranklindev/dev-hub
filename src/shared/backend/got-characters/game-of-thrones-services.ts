d4b9f7ba30cac303a3091973453eda077039cc4c
perdomofranklindev <perdomofranklindev@gmail.com>
2025-03-02 15:51:41 -0400

    Add Game of Thrones character data and service functions

--- src/shared/backend/got-characters/game-of-thrones-services.ts
+++ src/shared/backend/got-characters/game-of-thrones-services.ts
@@ -1,9 +1,10 @@
 import data from "./data.json";
 
 export const getGameOfThronesCharacters = (): Promise<typeof data> => {
+  const randomDelay = Math.floor(Math.random() * 3001); // Generates a random number between 0 and 3000 (inclusive)
   return new Promise((resolver, reject) => {
     setTimeout(() => {
       resolver(data);
-    }, 3000);
+    }, randomDelay);
   });
 };
