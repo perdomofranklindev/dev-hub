909c68017db89752a4c7246e273bf5bde13cd2de
perdomofranklindev <perdomofranklindev@gmail.com>
2025-03-23 18:04:24 -0400

    Add DarkModeSelection component for toggling dark mode in configuration sidebar

---
+++ src/shared/layouts/ConfigSidebar/DarkModeSelection.tsx
@@ -0,0 +1,24 @@
+import { IOSSwitch } from '@dev-hub/shared/components/IOSSwitch';
+import { Box, Typography, useColorScheme } from '@mui/material';
+import Brightness4 from '@mui/icons-material/Brightness4';
+
+const DarkModeSelection = () => {
+  const { mode, setMode } = useColorScheme();
+
+  const toggleColorMode = () => {
+    const newMode = mode === 'light' ? 'dark' : 'light';
+    setMode(newMode);
+  };
+
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Brightness4 sx={{ mr: 2, fontSize: 24 }} />
+      <Typography variant="body1" sx={{ flexGrow: 1 }}>
+        Dark Mode
+      </Typography>
+      <IOSSwitch checked={mode === 'dark'} onChange={toggleColorMode} color="secondary" />
+    </Box>
+  );
+};
+
+export default DarkModeSelection;
