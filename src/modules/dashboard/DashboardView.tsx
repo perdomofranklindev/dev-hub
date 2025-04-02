8f24dc48b331c0bc001a9d5fc467f5af5d926dda
perdomofranklindev <perdomofranklindev@gmail.com>
2025-04-02 08:48:48 -0400

    Enhance DashboardView with additional welcome message and layout adjustments

--- src/modules/dashboard/DashboardView.tsx
+++ src/modules/dashboard/DashboardView.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 
 const DashboardView: React.FC = () => {
   return (
-    <Box sx={{ pt: '64px', position: 'relative', width: '100%' }}>
+    <Box sx={{ position: 'relative', width: '100%' }}>
       <Box
         sx={{
           mx: 'auto',
           backgroundColor: 'primary.main',
           py: 2,
           px: 4,
-          width: '100%'
+          width: '100%',
         }}
       >
         <Typography variant="h4" sx={{ color: 'primary.contrastText' }}>
@@ -22,6 +22,11 @@ const DashboardView: React.FC = () => {
           You have some notifications
         </Typography>
       </Box>
+
+    <Typography variant="h3">
+      Welcome to your dashboard! Here you can find the latest updates, manage your tasks, and stay
+      informed about your notifications. Explore the features and make the most out of your experience.
+    </Typography>
     </Box>
   );
 };
