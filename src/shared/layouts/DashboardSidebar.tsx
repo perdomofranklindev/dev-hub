a8e2bb3d768f637f5e3765fb79bcd5bf14fc395f
perdomofranklindev <perdomofranklindev@gmail.com>
2025-04-02 08:52:26 -0400

    Refactor DashboardSidebar to use styled components for improved styling and layout

--- src/shared/layouts/DashboardSidebar.tsx
+++ src/shared/layouts/DashboardSidebar.tsx
@@ -1,16 +1,27 @@
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Toolbar from '@mui/material/Toolbar';
+import styled from '@mui/material/styles/styled';
 import { useSidebar } from './Sidebar/SidebarContext';
 import { SidebarSection } from './Sidebar/SidebarSection';
 import { menuOptions } from '../config/menuOptions';
 import { SidebarDrawer } from './Sidebar/SidebarDrawer';
 
+const Sidebar = styled(Box)(({theme}) => ({
+  gridArea: 'sidebar',
+  position: 'sticky',
+  height: '100vh',
+  overflowX: 'hidden',
+  overflowY: 'auto',
+  top: 0,
+  zIndex: theme.zIndex.drawer,
+}));
+
 const DashboardSidebar = () => {
   const { drawerWidth, isSidebarOpen, isMobile, onClose, enteringOnMobileMode } = useSidebar();
 
   return (
-    <Box gridArea="sidebar">
+    <Sidebar>
       <SidebarDrawer
         direction="left"
         width={drawerWidth}
@@ -28,7 +39,7 @@ const DashboardSidebar = () => {
           ))}
         </Box>
       </SidebarDrawer>
-    </Box>
+    </Sidebar>
   );
 };
 
